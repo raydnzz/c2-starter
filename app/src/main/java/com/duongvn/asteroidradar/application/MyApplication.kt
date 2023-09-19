336cd7a0d68842918b759e334e98f8b50119a972
PC <zzjohnenglish@gmail.com>
2023-09-19 20:16:20 +0700

    Fix comments

--- app/src/main/java/com/duongvn/asteroidradar/application/MyApplication.kt
+++ app/src/main/java/com/duongvn/asteroidradar/application/MyApplication.kt
@@ -1,6 +1,9 @@
 package com.duongvn.asteroidradar.application
 
+import android.annotation.SuppressLint
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
@@ -9,11 +12,20 @@ import java.util.concurrent.TimeUnit
 
 class MyApplication : Application() {
 
+    @SuppressLint("IdleBatteryChargingConstraints")
     override fun onCreate() {
         super.onCreate()
 
+        val constrains = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .setRequiresDeviceIdle(true)
+            .build()
+
         val uploadWorkRequest: WorkRequest =
             PeriodicWorkRequestBuilder<RefreshDataWorker>(24, TimeUnit.HOURS)
+                .setConstraints(constraints = constrains)
                 .build()
 
         WorkManager
